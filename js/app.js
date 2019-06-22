3029bfaec587745e91b8e74e29835fb8d2bee800
Michael Simpson <michael@simpsonindustries.co.uk>
2019-06-22 23:08:16 +0100

    Added service worker files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+if ("serviceWorker" in navigator) { 
+  window.addEventListener("load", function() {   
+    navigator.serviceWorker.register("/sw.js").then(
+      function(registration) { 
+        // Registration was successful
+        console.log("ServiceWorker registration successful with scope: ", registration.scope); }, 
+      function(err) { 
+        // registration failed :( 
+        console.log("ServiceWorker registration failed: ", err); 
+      }); 
+  })
+}
+
 $(document).ready(function() {
     $(".icon").click(function(){
         $(".modal-footer").html($("div", this).text());
         $(".modal-body").html($("img", this).clone());
         $('#modal').modal('toggle');
-    });
+    })
 });
