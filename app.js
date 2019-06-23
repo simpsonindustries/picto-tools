75f15d064c186bdc10c1e5e9f50bb22d5723e908
Michael Simpson <michael@simpsonindustries.co.uk>
2019-06-23 12:20:10 +0100

    restyled reworked...still needs work

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+if ("serviceWorker" in navigator) { 
+  window.addEventListener("load", function() {   
+    navigator.serviceWorker.register("/sw.js").then(
+      function(registration) { 
+        // Registration was successful
+        console.log("ServiceWorker registration successful with scope: ", registration.scope); }, 
+      function(err) { 
+        // registration failed :( 
+        console.log("ServiceWorker registration failed: ", err); 
+      }); 
+  })
+}
+
+    $(".icon").on("click touchstart", function(){
+        $(".modal-footer").html($("div", this).text());
+        $(".modal-body").html($("img", this).clone().removeClass("icon__svg").addClass("img-fluid"));
+        $('#modal').modal('toggle');
+    })
+});
