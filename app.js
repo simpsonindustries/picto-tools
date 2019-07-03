e6845c37f3b22e20f3718ef64e317bfb82baa123
Michael Simpson <michael@simpsonindustries.co.uk>
2019-07-04 00:17:27 +0100

    fixed performance issues

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
-if ("serviceWorker" in navigator) { 
-  window.addEventListener("load", function() {   
-    navigator.serviceWorker.register("/sw.js").then(
-      function(registration) { 
-        // Registration was successful
-        console.log("ServiceWorker registration successful with scope: ", registration.scope); }, 
-      function(err) { 
-        // registration failed :( 
-        console.log("ServiceWorker registration failed: ", err); 
-      }); 
-  });
-}
-
-    $("#modal").on("shown.bs.modal", function(event){
-        $(".modal-footer").html($(".icon__title", event.relatedTarget).text());
-        $(".modal-body").html($("img", event.relatedTarget).clone().removeClass("icon__svg").addClass("img-fluid"));
+  if ("serviceWorker" in navigator) { 
+    window.addEventListener("load", function() {   
+      navigator.serviceWorker.register("/sw.js").then(
+        function(registration) { 
+          // Registration was successful
+          console.log("ServiceWorker registration successful with scope: ", registration.scope); }, 
+        function(err) { 
+          // registration failed :( 
+          console.log("ServiceWorker registration failed: ", err); 
+        }); 
     });
+  }
+  
+  var $icons = $(".icon");
+  $.each($icons, function() {
+    $("img", $(this)).attr("src", "symbols/" + $("div", $(this)).text().replace(/ /g, "_") + ".svg");
+  });
+  
+  $("#modal").on("shown.bs.modal", function(event){
+    $(".modal-footer").html($(".icon__title", event.relatedTarget).text());
+    $(".modal-body").html($("img", event.relatedTarget).clone().removeClass("icon__svg").addClass("img-fluid"));
+  });
 });
